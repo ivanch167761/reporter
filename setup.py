ea15d4ab0c70953b9a048f91c16d08f6b1e8c791
ivanch167761 <iva.kulikov@gmail.com>
2023-09-27 13:17:28 +0200

    Initial project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='reporter', 
+        version='1.0', 
+        packages=find_packages(),
+        description='Python tool for listing files in a project folder and creating reports (in Microsoft Word format), merging all information.',
+        author='Ivan Kulikov',
+        author_email='iva.kulikov@gmail.com',
+    )
